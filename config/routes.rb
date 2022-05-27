55ac684986f01e532c24080c49fc04a04723b1b3
Katsu1001 <k.katsumi19991001@gmail.com>
2022-05-27 15:47:20 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'toppages/index'
   root to:'toppages#index'
+
+  get 'users/index'
+  get 'toppages/index'
+
+  resources :toppages, only: :index do
+    collection do
+      get 'privacy_policy'
+      get 'regulation'
+      get 'contact_us'
+      get 'administrator_info'
+    end
+  end
+
 end
