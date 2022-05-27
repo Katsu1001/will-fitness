d3c2fa5a9f35c7c9d810995c1522b5a7a9621777
Katsu1001 <k.katsumi19991001@gmail.com>
2022-05-27 17:11:26 +0900

    コントローラーの設定

--- app/controllers/columns_controller.rb
+++ app/controllers/columns_controller.rb
@@ -1,11 +1,25 @@
 class ColumnsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
+  end
+
+  def new
+    @column = Column.new
+  end
+
+  def create
+    @column = Column.new(column_params)
+   if @column.save
+      redirect_to columns_index_path
+    else
+     render :new
+   end
   end
 
   private
   def column_params
     params.require(:column).permit(:name, :introduction, :category_id, :part_id, :image).merge(user_id: current_user.id)
   end
-  
+
 end
