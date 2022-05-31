5e325f40b2690abbee1a2069fac7a3940191d91a
Katsu1001 <k.katsumi19991001@gmail.com>
2022-05-31 14:41:06 +0900

    目標管理のビューを作成(html, css, javaScript)

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   resources :toppages, only: :index do
     collection do
+      get 'to_do_list'
       get 'privacy_policy'
       get 'regulation'
       get 'contact_us'
